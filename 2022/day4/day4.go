4649b23abe54b322fcae031454bf4bc4d4221035
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-12-12 19:21:47 -0800

    [Go] Day 4, Part 1 complete

--- 2022/day4/day4.go
+++ 2022/day4/day4.go
@@ -6,15 +6,26 @@ func main() {
     pairs := inputdata.GetPairRanges()
     fmt.Println("Pairs:", len(pairs))
 
-    fullyContained := 0
+    completeOverlaps := 0
+    overlaps := 0
 
     for _, pair := range pairs {
         if (pair.Elf1.Min <= pair.Elf2.Min && pair.Elf1.Max >= pair.Elf2.Max) ||
            (pair.Elf2.Min <= pair.Elf1.Min && pair.Elf2.Max >= pair.Elf1.Max) {
-            fmt.Printf("Overlap: %d-%d and %d-%d\n", pair.Elf1.Min, pair.Elf1.Max, pair.Elf2.Min, pair.Elf2.Max)
-            fullyContained += 1
+            fmt.Printf("Complete Overlap: %d-%d and %d-%d\n", pair.Elf1.Min, pair.Elf1.Max, pair.Elf2.Min, pair.Elf2.Max)
+            completeOverlaps += 1
         }
+
+        if (pair.Elf1.Min >= pair.Elf2.Min && pair.Elf1.Min <= pair.Elf2.Max) ||
+           (pair.Elf1.Max >= pair.Elf2.Min && pair.Elf1.Max <= pair.Elf2.Max) ||
+           (pair.Elf2.Min >= pair.Elf1.Min && pair.Elf2.Min <= pair.Elf1.Max) ||
+           (pair.Elf2.Max >= pair.Elf1.Min && pair.Elf2.Max <= pair.Elf1.Max) {
+            fmt.Printf("Overlap: %d-%d and %d-%d\n", pair.Elf1.Min, pair.Elf1.Max, pair.Elf2.Min, pair.Elf2.Max)
+            overlaps += 1
+           }
     }
 
-    fmt.Println("Complete Overlaps:", fullyContained)
+    fmt.Println()
+    fmt.Println("Complete Overlaps:", completeOverlaps)
+    fmt.Println("Overlaps:", overlaps)
 }
