59e754ba44ed242057c0c0209faed2cd76488801
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-12-21 18:42:33 -0800

    Parsing file sizes and dir names

--- 2022/day7/day7.hs
+++ 2022/day7/day7.hs
@@ -14,13 +14,35 @@ getLog [filename] = readFile filename
 getLog ["--", filename] = readFile filename
 getLog _ = pure ""
 
-parseLog logLines = do
-  case logLines of
-    (line:remaining) -> do
-      parseLogLine line
-      parseLog remaining
-    [] -> pure ()
+parseLog [] = pure ()
+parseLog (('$':' ':command):logLines) = parseCommand (command, logLines)
+parseLog logLine = pure ()
 
-parseLogLine "" = pure ()
-parseLogLine line = do
-  putStrLn line
+parseCommand (('c':'d':' ':cd), logLines) = do
+  putStrLn ("CD: " ++ cd)
+  parseLog logLines
+
+parseCommand ("ls", logLines) = do
+  let (size, dirs, remaining) = parseDirListing (0, [], logLines)
+  putStrLn ("COMMAND: ls --> Files: " ++ (show size) ++ " | Dirs: " ++ (show (length dirs)))
+  parseLog remaining
+
+parseCommand (cmd, logLines) = do
+  putStrLn ("UNKNOWN COMMAND: " ++ cmd)
+  parseLog logLines
+
+parseDirListing :: (Int, [String], [String]) -> (Int, [String], [String])
+parseDirListing (dirSize, dirs, line : logLines) = do
+  let (newDirs, fileSize) = parseEntry (dirs, line)
+  let next = head logLines
+  let result = (dirSize + fileSize, newDirs, logLines)
+
+  case head logLines of
+    ('$':' ':_) -> result
+    _ -> parseDirListing result
+
+parseEntry :: ([String], [Char]) -> ([String], Int)
+parseEntry (dirs, ('d':'i':'r':' ':dir)) = (dir : dirs, 0)
+parseEntry (dirs, entry) = case break (== ' ') entry of
+  (size, _ : filename) -> (dirs, read size)
+  _ -> (dirs, 0)
