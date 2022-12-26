d1a85704a9d807d435e6409bd10f0a9ceef010e4
Jeff Handley <jeffhandley@users.noreply.github.com>
2022-12-26 11:14:06 -0800

    [Haskell] Begin day 8

---
+++ 2022/day8/day8.hs
@@ -0,0 +1,9 @@
+-- https://adventofcode.com/2022/day/8
+
+module Day8 where
+
+main = do
+  -- https://adventofcode.com/2022/day/8/input
+  inputData <- readFile "sample.txt"
+
+  putStrLn inputData
