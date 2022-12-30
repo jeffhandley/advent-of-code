5aacee95a97578273d196bd48a98a8ec1e37068d
= <=>
2022-12-29 21:54:19 -0800

    [F#] Day 9, Part 2 complete

--- 2022/day9/Program.fs
+++ 2022/day9/Program.fs
@@ -25,47 +25,66 @@ let parseMoveSeq (move: string) =
 // on each axis.
 let pullPos (h: Pos) (t: Pos) : Pos =
     match (h.X - t.X, h.Y - t.Y) with
-    | (x, y) when abs x <= 1 && abs y <= 1 -> t
-    | (x, y) -> movePos (x.CompareTo(0)) (y.CompareTo(0)) t
+    | (x,y) when abs x <= 1 && abs y <= 1 -> t
+    | (x,y) -> movePos (x.CompareTo(0)) (y.CompareTo(0)) t
 
-// Create the Head and Tail positions both starting at (0,0).
-let mutable H = Pos(0,0)
-let mutable T = H
+let processRopeMoves (knotCount: int) (verbosity: int) =
+    // Create the rope with all knot positions initialized to (0,0).
+    let mutable knots = Array.create knotCount (Pos(0, 0))
 
-// Create lists for tracking the cells visited by both the Head and Tail.
-let mutable visitedH = [H]
-let mutable visitedT = [T]
+    // Track the visits for all knots in the rope.
+    let mutable visits = Array.create knotCount [Pos (0, 0)]
 
-// For each line in the input file, parse the line into a sequence of moves.
-// Then execute those moves and track the head and tail visits.
-for line in File.ReadAllLines("input.txt") do
-    // printfn "MOVE \tHEAD         \tVisits\tTAIL         \tVisits"
-    // printfn "%-5s\t(%+5d,%+5d)\t%-6d\t(%+5d,%+5d)\t%-6d" line H.X H.Y (List.distinct visitedH).Length T.X T.Y (List.distinct visitedT).Length
+    // For each line in the input file, parse the line into a sequence of moves.
+    // Then execute those moves and track the head and tail visits.
+    for line in File.ReadAllLines("input.txt") do
+        let (H, T) = (Array.head knots, Array.last knots)
 
-    for move in parseMoveSeq line do
-        H <- move H
-        T <- pullPos H T
+        if verbosity > 1 then
+            printfn "MOVE \tHEAD         \tVisits\tTAIL         \tVisits"
+            printfn "%-5s\t(%+5d,%+5d)\t%-6d\t(%+5d,%+5d)\t%-6d" line H.X H.Y (List.distinct (Array.head visits)).Length T.X T.Y (List.distinct (Array.last visits)).Length
 
-        visitedH <- H :: visitedH
-        visitedT <- T :: visitedT
+        for move in parseMoveSeq line do
+            Array.set knots 0 (move knots[0])
+            Array.set visits 0 (knots[0] :: visits[0])
 
-        //printfn "  -->\t(%+5d,%+5d)\t%-6d\t(%+5d,%+5d)\t%-6d" H.X H.Y (List.distinct visitedH).Length T.X T.Y (List.distinct visitedT).Length
-    //printfn ""
+            for k in 1 .. (Array.length knots - 1) do
+                Array.set knots k (pullPos knots[k - 1] knots[k])
+                Array.set visits k (knots[k] :: visits[k])
 
-(*
-let xs = List.map (fun (p: Pos) -> p.X) visitedT
-let ys = List.map (fun (p: Pos) -> p.Y) visitedT
+            if verbosity > 1 then
+                printfn "  -->\t(%+5d,%+5d)\t%-6d\t(%+5d,%+5d)\t%-6d" H.X H.Y (List.distinct (Array.head visits)).Length T.X T.Y (List.distinct (Array.last visits)).Length
 
-for y in (List.min ys) .. (List.max ys) do
-    for x in (List.min xs) .. (List.max xs) do
-        printf (
-                if H.X = x && H.Y = y then "H"
-                elif T.X = x && T.Y = y then "T"
-                elif List.contains (Pos(x, y)) visitedT then "#"
-                else "."
-        )
-    printfn ""
-*)
+        if verbosity > 1 then
+            printfn ""
 
-printfn "DONE!\tHEAD         \tVisits\tTAIL         \tVisits"
-printfn "     \t(%+5d,%+5d)\t%-6d\t(%+5d,%+5d)\t%-6d" H.X H.Y (List.distinct visitedH).Length T.X T.Y (List.distinct visitedT).Length
+    let (H, T) = (Array.head knots, Array.last knots)
+
+    if verbosity > 0 then
+        let tailVisits = (Array.last visits)
+        let xs = List.map (fun (p: Pos) -> p.X) tailVisits
+        let ys = List.map (fun (p: Pos) -> p.Y) tailVisits
+
+        for y in (List.min ys) .. (List.max ys) do
+            for x in (List.min xs) .. (List.max xs) do
+                printf (
+                    if   H.X = x && H.Y = y then "H"
+                    elif T.X = x && T.Y = y then "T"
+                    elif   0 = x &&   0 = y then "s"
+                    elif List.contains (Pos(x, y)) tailVisits then "#"
+                    else "."
+                )
+            printfn ""
+
+        printfn ""
+        printfn "DONE!\tHEAD         \tVisits\tTAIL         \tVisits"
+        printfn "     \t(%+5d,%+5d)\t%-6d\t(%+5d,%+5d)\t%-6d" H.X H.Y (List.distinct (Array.head visits)).Length T.X T.Y (List.distinct (Array.last visits)).Length
+        printfn ""
+
+    visits
+
+let partOneVisits = processRopeMoves  2 1
+let partTwoVisits = processRopeMoves 10 1
+
+printfn "[PART ONE ANSWER] %d" ((List.distinct (Array.last partOneVisits)).Length)
+printfn "[PART TWO ANSWER] %d" ((List.distinct (Array.last partTwoVisits)).Length)
